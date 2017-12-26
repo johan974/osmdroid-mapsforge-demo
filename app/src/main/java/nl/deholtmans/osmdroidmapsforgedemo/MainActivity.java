438bbab3b300d6f48b593ed32e0ad9e8ca7466b2
johan974 <jj974@upcmail.nl>
2017-12-26 12:49:52 +0100

    Osmdroid - basic version using Mapnik. Error: D/OsmDroid: Unable to store cached tile from Mapnik x/y/z, database not available. After solving this, the next step is exchanging the Tilesource with the Mapsforge Tilesource.

---
+++ app/src/main/java/nl/deholtmans/osmdroidmapsforgedemo/MainActivity.java
@@ -0,0 +1,61 @@
+package nl.deholtmans.osmdroidmapsforgedemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    private MapController mapController;
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setBuiltInZoomControls(true);
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(17);
+        GeoPoint startPoint = new GeoPoint(52.2222, 6.6123);
+        mapController.setCenter(startPoint);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
